69cc4de8cc4d4cf0b60fc32cdd1f29566c3ba3d1
xcSOFT <rusl1n.rutube@gmail.com>
2026-04-06 15:42:09 +0300

    Update FLK.lua
    fix2

--- FLK.lua
+++ FLK.lua
@@ -520,7 +520,7 @@ local function toggleGui()
             if isRussian then
                 player:Chat("/me Нажмите ["..keyStr.."] чтобы открыть меню")
             else
-                player:Chat("/me Press ["..keyStr.."] to open menu")
+                player:Chat("/me Press ["..keyStr.."] to reopen menu")
             end
         end)
     end
